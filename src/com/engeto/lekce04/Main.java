88983a7e1b58317e46299ae66018a7429604bd29
Martin Šimůnek <public@e-mis.cz>
2022-08-03 19:34:33 +0200

    Pridan vypis dnesniho data
    Po osloveni vypisi dnesni datum pomoci LocalDate.now().

--- src/com/engeto/lekce04/Main.java
+++ src/com/engeto/lekce04/Main.java
@@ -1,7 +1,10 @@
 package com.engeto.lekce04;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Karel!");
+        System.out.println("Dnes je: "+ LocalDate.now());
     }
 }
